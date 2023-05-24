57db960fd39b47b9fbdbb024217efe4f0ecf2d2b
choihansu97 <jjpp9712@naver.com>
2023-05-25 02:06:45 +0900

    feat(main):메인 페이지 작업

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const API_KEY = 'a6e1ddf468ee3659c8c52bb9c0f4413d';
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/'
